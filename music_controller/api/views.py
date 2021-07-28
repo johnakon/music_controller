6fe6d316f6d60e5e8b7584e944ab9fa5c72777e7
johnakon <john.akonya@gmail.com>
2021-07-28 18:54:06 +0300

    RoomView class to display json from serializer class

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,6 +1,15 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
 
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
+
 # Create your views here.
-def welcome(request):
-    return HttpResponse("<h2>hello there</h2>")
+
+# def welcome(request):
+#     return HttpResponse("<h2>hello there</h2>")
+
+# class RoomView(generics.CreateAPIView):
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
